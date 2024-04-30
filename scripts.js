26f85e0a32b5e5cbf96d1c3e7d3dc78eb2002eb2
Vinícius Lima <vinicius43333@gmail.com>
2024-04-29 23:40:20 -0300

    feat: Adde sounds and word changing #2

--- scripts.js
+++ scripts.js
@@ -1,21 +1,49 @@
+var keySound = new Audio('./src/sounds/keySound.mp3');
+var errorSound = new Audio('./src/sounds/errorSound.mp3');
+var correctWordSound = new Audio('./src/sounds/correctWordSound.mp3');
+
 var current_word_div = document.getElementById('word');
 
 document.addEventListener('keypress', handleKeyPress);
 document.addEventListener('keydown', handleBackspace);
 
-var current_word = 'word';
+var correctCounter = 0;
+var errorCounter = 0;
+
+var wordList = [
+  'esteganografia',
+  'vinicius',
+  'poesia',
+  'alucinação',
+  'tripophobia',
+  'manikeismo',
+  'hiperventilação',
+  'musician',
+  'facility',
+  'human'
+];
+
+var wordListPosition = 0;
+var current_word = wordList[wordListPosition];
 var typed_word = '';
 
-current_word_div.innerHTML = [...current_word].map((letter, i) => {
-  return `<span id='${i}'>` + letter + '</span>'
-}).join('');
+getNextWord();
 
 function handleKeyPress(e){
   if(current_word[typed_word.length] === e.key){
     updateLetter(String(typed_word.length), 'correct');
+    playSound(keySound);
+    correctCounter += 1;
     typed_word += e.key;
+
+    if(current_word.length === typed_word.length){
+      playSound(correctWordSound);
+      getNextWord();
+    };
   } else {
+    errorCounter += 1;
     updateLetter(String(typed_word.length), 'wrong');
+    playSound(errorSound);
   };
 };
 
@@ -33,4 +61,20 @@ function updateLetter(id, status){
     current_letter.style.color = '#4caf50';
   else if( status === 'wrong')
     current_letter.style.color = '#f44336';
+};
+
+function getNextWord(){
+  current_word = wordList[wordListPosition];
+  wordListPosition += 1;
+  typed_word = '';
+
+  current_word_div.innerHTML = [...current_word].map((letter, i) => {
+    return `<span id='${i}'>` + letter + '</span>'
+  }).join('');
+};
+
+function playSound(sound){
+  sound.pause();
+  sound.currentTime = 0;
+  sound.play();
 };
